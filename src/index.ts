df0946440b904012318d453349bc790f574f61c0
ujjwal-kr <katanadeveloper01@gmail.com>
2020-03-29 00:08:30 +0530

    init

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import {StoreApplication} from './application';
+import {ApplicationConfig} from '@loopback/core';
+
+export {StoreApplication};
+
+export async function main(options: ApplicationConfig = {}) {
+  const app = new StoreApplication(options);
+  await app.boot();
+  await app.start();
+
+  const url = app.restServer.url;
+  console.log(`Server is running at ${url}`);
+  console.log(`Try ${url}/ping`);
+
+  return app;
+}
